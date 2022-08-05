d4804bbbdef1dbe05eaa72285530edef49fcd439
nicoliprogramer <imotivacionesf@gmail.com>
2022-08-05 08:22:23 -0300

    acomodando

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
-fetch("data.json")
+const lista = document.getElementById("lista");
+
+
+fetch("./public/data.json", { mode: "no-cors" })
     .then(resp => resp.json())
-    .then(productos => {
-        console.log(productos);
-        productos.forEach(producto => {
+    .then(preparativos => {
+        console.log(preparativos);
+        preparativos.forEach(preparativo => {
             const li = document.createElement('li');
             li.innerHTML = `
-                <h2>${producto.nombre}</h2>
-                <p>${producto.precio}</p>
-                <p>codigo: ${producto.id}</p>
-                <p>${producto.img}</p>
+                <h2>${preparativo.nombre}</h2>
+                <p>codigo: ${preparativo.id}</p>
+                <p>${preparativo.img}</p>
                 <hr/>
             `;
             lista.append(li);
         });
-    }).catch(error => console.log(error))
+    })
